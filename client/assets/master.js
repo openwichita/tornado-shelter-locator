4ba604eec37680ba7738e911427ee0f4f86cb3ca
Austin Crane <amcrane1@wichita.edu>
2016-10-14 11:16:10 -0500

    added google maps

--- client/assets/master.js
+++ client/assets/master.js
@@ -1,1 +1,8 @@
 // all scripts for app that dont have to do with network calls
+
+// initialize google map
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4
+  });
+}
