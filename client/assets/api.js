d519d54af32e65fd41d8bd7b24dd5ccd4731429f
cjprieb <cjprieb@gmail.com>
2016-10-31 20:03:34 -0500

    Added a list display of shelters to the page

--- client/assets/api.js
+++ client/assets/api.js
@@ -1,10 +1,41 @@
-// all scripts for api calls with ajax
-var api_url = 'CHANGE ME';
+$(function() {
+  // all scripts for api calls with ajax
+  var api_url = 'assets/test-data.json'; //TODO: CHANGE ME!
 
-function getAllShelters(success, fail) {
-  console.log('function not implemented yet');
-}
+  function getAllShelters(success, fail) {
+    console.log('function not implemented yet');
+  }
 
-function getShelterById(id, success, fail) {
-  console.log('function not implemented yet');
-}
+  function getShelterById(id, success, fail) {
+    console.log('function not implemented yet');
+  }
+
+  function ShelterData(data) {
+    var self = this;
+
+    self.originalShelters = data; 
+
+    //Not the prettiest way of doing it, but it works.
+    var columnLength = Math.floor(data.length / 3);
+    var column1 = data;
+    var column2 = data.splice(0, columnLength);
+    var column3 = data.splice(0, columnLength);
+    self.shelterGroups = ko.observableArray([column1, column2, column3]);
+  }
+
+  ko.applyBindings(new ShelterData(ALL_SHELTERS));
+  //TODO: when client can be run through a server and not the file system
+  // var shelterUrl = api_url;
+  // $.ajax({
+  //   url : shelterUrl,
+  //   type: 'GET',
+  //   success : function(data) {
+  //     console.log("found data", data);
+  //       ko.applyBindings(new ShelterData(data));
+
+  //       $("#loading").addClass("hidden");
+  //       $("#shelter-list").removeClass("hidden");
+  //     }
+  // }); 
+
+});
